3e3a04ee1b96fd9f6bad6c06414553dd143a78d5
ShefaaAbed <101830221+ShefaaAbed@users.noreply.github.com>
2022-05-05 16:06:21 +0300

    Initial Timeline Comonent

--- src/components/Timeline.jsx
+++ src/components/Timeline.jsx
@@ -1,6 +1,25 @@
 import React from 'react';
+import { Card, Timeline } from 'antd';
 
-function Timeline() {
-	return <h1>this is a timeline</h1>;
+function TimelineComponent() {
+	return (
+		<div>
+			<Card style={{ backgroundColor: '#F7F7F7' }}>
+				<h1>TimeLine</h1>
+				<hr />
+				<Timeline>
+					<Timeline.Item>Create a Requirement File 2022-01-15</Timeline.Item>
+					<Timeline.Item>Create a Design File 2022-02-07</Timeline.Item>
+					<Timeline.Item>
+						<p>Design the database</p>
+						<p>Design the user interface 2022-05-15</p>
+					</Timeline.Item>
+					<Timeline.Item>
+						Deliver the full web application 2022-06-30
+					</Timeline.Item>
+				</Timeline>
+			</Card>
+		</div>
+	);
 }
-export default Timeline;
+export default TimelineComponent;
--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import Timeline from '../../components/Timeline';
+import TimelineComponent from '../../components/Timeline';
 
 function Dashboard() {
-	return <Timeline />;
+	return <TimelineComponent />;
 }
 export default Dashboard;
