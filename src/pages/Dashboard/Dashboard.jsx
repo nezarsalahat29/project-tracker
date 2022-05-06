634cc0e3e261b60e69fb463b7f17da3f2a471341
ShefaaAbed <101830221+ShefaaAbed@users.noreply.github.com>
2022-05-07 00:59:34 +0300

    Updates

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,30 +1,32 @@
-import React from "react";
-import Inbox from "../../components/Dashboard/Inbox";
-import LastModifiedTable from "../../components/Dashboard/LastModifiedTable";
-import StatisticsBar from "../../components/Dashboard/Statistics";
-import TimelineComponent from "../../components/Dashboard/Timeline";
-import TaskDonut from "../../components/Dashboard/TaskDonut";
-import LiquidPlot from "../../components/Dashboard/Liquid";
-import { useAuth } from "../../contexts/AuthContext";
+import React from 'react';
+import Inbox from '../../components/Dashboard/Inbox';
+import LastModifiedTable from '../../components/Dashboard/LastModifiedTable';
+import StatisticsBar from '../../components/Dashboard/Statistics';
+import TimelineComponent from '../../components/Dashboard/Timeline';
+import TaskDonut from '../../components/Dashboard/TaskDonut';
+import LiquidPlot from '../../components/Dashboard/Liquid';
+import { useAuth } from '../../contexts/AuthContext';
 const instructorDashboard = [
-  <StatisticsBar />,
-  <TimelineComponent />,
-  <LastModifiedTable />,
-  <Inbox />,
-  <LiquidPlot />,
+	<StatisticsBar />,
+	<TimelineComponent />,
+	<LastModifiedTable />,
+	<Inbox />,
+	<LiquidPlot />,
 ];
 const studentDashboard = [
-  <TimelineComponent />,
-  <TaskDonut />,
-  <Inbox />,
-  <LiquidPlot />,
+	<TimelineComponent />,
+	<TaskDonut />,
+	<Inbox />,
+	<LiquidPlot />,
 ];
 
 function Dashboard() {
-  const { currentUser } = useAuth();
-  const items = currentUser.instructor ? instructorDashboard : studentDashboard;
+	const { currentUser } = useAuth();
+	const items = currentUser.instructor
+		? instructorDashboard
+		: instructorDashboard;
 
-  return <div>{items}</div>;
+	return <div>{items}</div>;
 }
 
 export default Dashboard;
