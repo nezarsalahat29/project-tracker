2370141dcc415e838fc4c1fadc8ea733e1b2bee7
ShefaaAbed <101830221+ShefaaAbed@users.noreply.github.com>
2022-05-08 18:44:41 +0300

    Project screen finished

--- src/pages/Project/index.jsx
+++ src/pages/Project/index.jsx
@@ -1,6 +1,55 @@
 import React from 'react';
+import { Card } from 'antd';
+import { Divider } from 'antd';
+import { Typography, Space } from 'antd';
+import { Progress, Button } from 'antd';
+import { MinusOutlined, PlusOutlined } from '@ant-design/icons';
 
+const { Text, Link } = Typography;
+
+const { Title } = Typography;
 function Project() {
-	return <h1>This is a Project</h1>;
+	const state = {
+		percent: 0,
+	};
+	const increase = () => {
+		let percent = this.state.percent + 10;
+		if (percent > 100) {
+			percent = 100;
+		}
+		this.setState({ percent });
+	};
+	const decline = () => {
+		let percent = this.state.percent - 10;
+		if (percent < 0) {
+			percent = 0;
+		}
+		this.setState({ percent });
+	};
+	return (
+		<div>
+			<Card style={{ backgroundColor: '#F7F7F7' }}>
+				<Title level={2}>Project</Title>
+				<Divider />
+				<p>
+					Lorem ipsum dolor sit, amet consectetur adipisicing elit. Enim rem
+					eaque voluptatem, similique voluptas, eligendi ullam aspernatur itaque
+					sunt ratione est voluptatibus laudantium, veritatis doloremque fugiat
+					officiis debitis repellendus nostrum! Lorem ipsum dolor sit, amet
+					consectetur adipisicing elit. Enim rem eaque voluptatem, similique
+					voluptas, eligendi ullam aspernatur itaque sunt ratione est
+					voluptatibus laudantium, veritatis doloremque fugiat officiis debitis
+					repellendus nostrum!
+				</p>
+				<Space direction='vertical'>
+					<Text strong>Due Date: 30/5/2022</Text>
+				</Space>
+				<Progress
+					strokeColor={{ '0%': '#108ee9', '100%': '#87d068' }}
+					percent={59.5}
+				/>
+			</Card>
+		</div>
+	);
 }
 export default Project;
