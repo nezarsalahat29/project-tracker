db9b2b810a8f3fd4ff9d9a1a564db02922f9c878
MohanadMakhzoumi <m.makhzome@gmail.com>
2022-05-20 17:35:20 +0300

    progress size

--- src/pages/Project/index.jsx
+++ src/pages/Project/index.jsx
@@ -5,7 +5,7 @@
 import React, { useState } from "react";
 import "./index.css";
 import { Liquid } from "@ant-design/plots";
-import LiquidPlot from "../../components/Dashboard/Liquid"
+import LiquidPlot from "../../components/Dashboard/Liquid";
 import TasksLists from "../Tasks";
 import { Typography, Button, Layout, Menu, Breadcrumb, Image } from "antd";
 import {
@@ -21,12 +21,29 @@ import { DownloadOutlined } from "@ant-design/icons";
 import { Row, Col, Divider } from "antd";
 import { Progress } from "antd";
 import { Descriptions } from "antd";
-import { Drawer, Form, Input, Select, DatePicker, Space } from 'antd';
+import { Drawer, Form, Input, Select, DatePicker, Space } from "antd";
 const { Meta } = Card;
 const { Header, Content, Footer, Sider } = Layout;
 const { Title } = Typography;
 /****************************************************************/
 const { option } = Select;
+
+function LiquidP() {
+  const config = {
+    percent: 0.7,
+    outline: {
+      distance: 8,
+    },
+    wave: {
+      length: 130,
+    },
+  };
+  return (
+    <div style={{ width: 150, height: 140 }}>
+      <Liquid {...config} />
+    </div>
+  );
+}
 export default function Taskk() {
   const [visible, setVisible] = React.useState(false);
 
@@ -37,7 +54,6 @@ export default function Taskk() {
   const onClose = () => {
     setVisible(false);
   };
-
 
   /************************************************************* */
   function getItem(label, key, icon, children) {
@@ -62,62 +78,78 @@ export default function Taskk() {
     },
   ];
 
-
   return (
     <>
       <Row gutter={26}>
         {Projects.map((e) => {
           return (
             <Row>
-              <Divider orientation="center" style={{ fontWeight: "bolder", fontSize: "40px", fontFamily: "cursive" }}>{e.Title}</Divider>
+              <Divider
+                orientation="center"
+                style={{
+                  fontWeight: "bolder",
+                  fontSize: "40px",
+                  fontFamily: "cursive",
+                }}
+              >
+                {e.Title}
+              </Divider>
               <div className="site-card-border-less-wrapper">
                 <Card
                   headStyle={{ fontFamily: "-moz-initial", fontSize: "25px" }}
                   title={e.description}
                   bordered={false}
                   style={{ width: 800 }}
-                  bodyStyle={{ fontWeight: "bold", fontFamily: "-moz-initial", fontSize: "20px" }}
+                  bodyStyle={{
+                    fontWeight: "bold",
+                    fontFamily: "-moz-initial",
+                    fontSize: "20px",
+                  }}
                 >
                   <Row>
                     <Col span={12}>
                       <p>Start Date: {e.startDate}</p>
+                      
                     </Col>
                     <Col span={12}>
                       <p>End Date: {e.endDAte}</p>
+                      <LiquidP/>
                     </Col>
-
+                    
                   </Row>
-
-                  <p>ID: {e.id}</p>
-
-                  <Col span={18}>
-                    <LiquidPlot />
-                  </Col>
-
-
-
-
-
-
-
+                  
+                    
+                    <p>ID: {e.id}</p>
+                    
+              
+                    
+                  
+                  
+                  
                 </Card>
               </div>
-              <Progress width={20} percent={e.Progress * 100} />
+              
             </Row>
-
-
           );
         })}
       </Row>
 
-
       <Row justify="end">
-
         <Col span={4}>
           {" "}
-          <Button type="primary" onClick={showDrawer} style={{ backgroundColor: "#002766", borderColor: "#002766", borderRadius: "500" }}
+          <Button
+            type="primary"
+            onClick={showDrawer}
+            style={{
+              backgroundColor: "#002766",
+              borderColor: "#002766",
+              borderRadius: "500",
+            }}
             icon={
-              <AppstoreAddOutlined style={{ fontSize: 20, fontWeight: "bold", color: "white" }} />}
+              <AppstoreAddOutlined
+                style={{ fontSize: 20, fontWeight: "bold", color: "white" }}
+              />
+            }
           >
             Add New Task
           </Button>
@@ -142,7 +174,9 @@ export default function Taskk() {
                   <Form.Item
                     name="Task Title"
                     label="Task Title: "
-                    rules={[{ required: true, message: 'Please Enter Task Title: ' }]}
+                    rules={[
+                      { required: true, message: "Please Enter Task Title: " },
+                    ]}
                   >
                     <Input placeholder="Please Enter Task Title" />
                   </Form.Item>
@@ -153,7 +187,12 @@ export default function Taskk() {
                   <Form.Item
                     name="Project Name"
                     label="Project Name: "
-                    rules={[{ required: true, message: 'Please Select Project Name: ' }]}
+                    rules={[
+                      {
+                        required: true,
+                        message: "Please Select Project Name: ",
+                      },
+                    ]}
                   >
                     <Select placeholder=" Project Name:">
                       <option value="xiao">Project 1 </option>
@@ -167,7 +206,9 @@ export default function Taskk() {
                   <Form.Item
                     name="Project Leader"
                     label="Project Leader: "
-                    rules={[{ required: true, message: 'Select Project Leader' }]}
+                    rules={[
+                      { required: true, message: "Select Project Leader" },
+                    ]}
                   >
                     <Input placeholder="Please Enter Project Leadr Name" />
                   </Form.Item>
@@ -175,16 +216,17 @@ export default function Taskk() {
               </Row>
 
               <Row gutter={16}>
-
                 <Col span={12}>
                   <Form.Item
                     name="DateTime"
                     label="DateTime"
-                    rules={[{ required: true, message: 'Please choose the DateTime' }]}
+                    rules={[
+                      { required: true, message: "Please choose the DateTime" },
+                    ]}
                   >
                     <DatePicker.RangePicker
-                      style={{ width: '100%' }}
-                      getPopupContainer={trigger => trigger.parentElement}
+                      style={{ width: "100%" }}
+                      getPopupContainer={(trigger) => trigger.parentElement}
                     />
                   </Form.Item>
                 </Col>
@@ -197,7 +239,7 @@ export default function Taskk() {
                     rules={[
                       {
                         required: true,
-                        message: 'Please Enter Task Description',
+                        message: "Please Enter Task Description",
                       },
                     ]}
                   >
@@ -207,16 +249,9 @@ export default function Taskk() {
               </Row>
             </Form>
           </Drawer>
-
         </Col>
         <TasksLists />
       </Row>
-
-
-
-
     </>
   );
-
 }
-
