740411f210daae4f061f36022b48e6bcbeaf521f
JEBREEL MAJDALWIEH <jemajdalwieh18@cit.just.edu.jo>
2022-05-20 19:49:13 +0300

    hhh

--- src/pages/Navbar/index.jsx
+++ src/pages/Navbar/index.jsx
@@ -1,21 +1,20 @@
-import React, { useState } from 'react';
-import { useAuth } from '../../contexts/AuthContext';
-import './index.css';
-import { Button, Layout, Menu, Image, Affix, Avatar } from 'antd';
+import React, { useState } from "react";
+import { useAuth } from "../../contexts/AuthContext";
+import "./index.css";
+import { Button, Layout, Menu, Image, Affix, Avatar } from "antd";
 import {
   DesktopOutlined,
   PieChartOutlined,
   FileOutlined,
   TeamOutlined,
-} from '@ant-design/icons';
-import { Routes, Route, Link } from 'react-router-dom';
-import Dashboard from '../Dashboard';
-import Class from '../Class';
-import FaultPage from '../faultPage';
-import Chat from '../Chat';
-import Project from '../Project';
-import TasksLists from '../Tasks';
-
+} from "@ant-design/icons";
+import { Routes, Route, Link } from "react-router-dom";
+import Dashboard from "../Dashboard";
+import Class from "../Class";
+import FaultPage from "../faultPage";
+import Chat from "../Chat";
+import Project from "../Project";
+import TasksLists from "../Tasks";
 
 const { Header, Content, Sider } = Layout;
 
@@ -29,16 +28,16 @@ function getItem(label, key, icon, children) {
 }
 
 const instructorItems = [
-  getItem(<Link to='/'>Dashboard</Link>, '1', <PieChartOutlined />),
-  getItem(<Link to='/Class'>Class</Link>, '2', <DesktopOutlined />),
-  getItem(<Link to='/Chat'>Chat</Link>, '6', <FileOutlined />),
-  getItem(<Link to='/Project'>Project</Link>, '4', <TeamOutlined />),
+  getItem(<Link to="/">Dashboard</Link>, "1", <PieChartOutlined />),
+  getItem(<Link to="/Class">Class</Link>, "2", <DesktopOutlined />),
+  getItem(<Link to="/Chat">Chat</Link>, "6", <FileOutlined />),
+  getItem(<Link to="/Project">Project</Link>, "4", <TeamOutlined />),
 ];
 
 const studentItems = [
-  getItem(<Link to='/'>Dashboard</Link>, '1', <PieChartOutlined />),
-  getItem(<Link to='/Project'>Project</Link>, '4', <TeamOutlined />),
-  getItem(<Link to='/Chat'>Chat</Link>, '6', <FileOutlined />),
+  getItem(<Link to="/">Dashboard</Link>, "1", <PieChartOutlined />),
+  getItem(<Link to="/Project">Project</Link>, "4", <TeamOutlined />),
+  getItem(<Link to="/Chat">Chat</Link>, "6", <FileOutlined />),
 ];
 
 export default function Navbar() {
@@ -49,72 +48,72 @@ export default function Navbar() {
   return (
     <Layout
       style={{
-        minHeight: '100vh',
+        minHeight: "100vh",
       }}
     >
       <Affix offsetTop={0}>
         <Sider
-          style={{ height: '100vh' }}
+          style={{ height: "100vh" }}
           collapsible
           collapsed={collapsed}
           onCollapse={() => setCollapsed(!collapsed)}
         >
           <Image
-            className='logo'
-            src={process.env.PUBLIC_URL + '/logo_transparent.png'}
+            className="logo"
+            src={process.env.PUBLIC_URL + "/logo_transparent.png"}
             preview={false}
           />
           <Menu
-            theme='dark'
-            defaultSelectedKeys={['1']}
-            mode='inline'
+            theme="dark"
+            defaultSelectedKeys={["1"]}
+            mode="inline"
             items={items}
           />
         </Sider>
       </Affix>
-      <Layout className='site-layout'>
+      <Layout className="site-layout">
         <Header
-          className='site-layout-background'
+          className="site-layout-background"
           style={{
-            display: 'flex',
-            justifyContent: 'space-between',
-            alignItems: 'center',
-            borderBottom: '1px solid rgba(240, 240, 240)',
+            display: "flex",
+            justifyContent: "space-between",
+            alignItems: "center",
+            borderBottom: "1px solid rgba(240, 240, 240)",
           }}
         >
           <Avatar
             style={{
-              backgroundColor: '#1890ff',
-              verticalAlign: 'middle',
+              backgroundColor: "#1890ff",
+              verticalAlign: "middle",
             }}
-            size='large'
+            size="large"
             gap={4}
           >
             {currentUser.name[0].toUpperCase()}
           </Avatar>
-          <Button type='primary' onClick={logout}>
+          <Button type="primary" onClick={logout}>
             Sign Out
           </Button>
         </Header>
         <Content
           style={{
-            margin: '0 16px',
+            margin: "0 16px",
           }}
         >
           <div
-            className='site-layout-background'
+            className="site-layout-background"
             style={{
               padding: 24,
-              minHeight: '100%',
+              minHeight: "100%",
             }}
           >
             <Routes>
-              <Route path='/' element={<Dashboard />} />
-              <Route path='/Class' element={<Class />} />
-              <Route path='/Chat' element={<Chat />} />
-              <Route path='*' element={<FaultPage />} />
-              <Route path='/Project' element={<Project />} />
-              <Route path='/Tasks' element={<TasksLists />} />
+              <Route path="/" element={<Dashboard />} />
+              <Route path="/Class" element={<Class />} />
+              <Route path="/Chat" element={<Chat />} />
+              <Route path="*" element={<FaultPage />} />
+              <Route path="/Project" element={<Project />} />
+              <Route path="/Tasks" element={<TasksLists />} />
             </Routes>
           </div>
         </Content>
