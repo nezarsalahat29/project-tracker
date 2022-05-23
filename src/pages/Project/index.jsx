3493402dc1557c17f9bc3b0ebf14b22e69cc1305
JEBREEL MAJDALWIEH <jemajdalwieh18@cit.just.edu.jo>
2022-05-23 23:35:12 +0300

    noGroupAssigned Warnning

--- src/pages/Project/index.jsx
+++ src/pages/Project/index.jsx
@@ -7,7 +7,7 @@ import "./index.css";
 import { Liquid } from "@ant-design/plots";
 import TasksLists from "../Tasks";
 import { Button } from "antd";
-
+import { Alert } from 'antd';
 import { AppstoreAddOutlined } from "@ant-design/icons";
 import { Card } from "antd";
 
@@ -15,7 +15,7 @@ import { Row, Col, Collapse, Radio } from "antd";
 
 import { Drawer, Form, Input, Select, DatePicker, Space } from "antd";
 import Calendar from "../../components/Dashboard/Calendar";
-import { Avatar, List } from "antd";
+import { List } from "antd";
 
 import { TeamOutlined } from "@ant-design/icons";
 /****************************************************************/
@@ -39,6 +39,7 @@ function LiquidP() {
 export default function Project() {
   const [Taskvisible, setTaskVisible] = React.useState(false);
   const [Groupvisible, setGroupVisible] = React.useState(false);
+  
 
   const showTaskDrawer = () => {
     setTaskVisible(true);
@@ -62,198 +63,191 @@ export default function Project() {
     { id: 133052, title: "Hedaya" },
     { id: 133053, title: "Mohannad" },
   ];
+  <Alert message="Informational Notes" type="info" showIcon />
 
-  /************************************************************* */
-
-  const Projects = 
-    {
-      id: 1,
-      description:
-        "You will find here the description of this project and other details",
-      startDate: "12-Jul-2022",
-      endDAte: "7-Aug-2022",
-      Title: "Project Name",
-      tasks: ["task1", "task2", "task3"],
-      Progress: 0.6,
-      deliverables: ["dev1", "dev2", "dev3"],
-    }
-  ;
-
+  const Projects = {
+    id: 1,
+    description:
+      "You will find here the description of this project and other details",
+    startDate: "12-Jul-2022",
+    endDAte: "7-Aug-2022",
+    Title: "Project Name",
+    tasks: ["task1", "task2", "task3"],
+    Progress: 0.6,
+    deliverables: ["dev1", "dev2", "dev3"],
+  };
   return (
     <>
+      <div>
+        <h1>{Projects.Title}</h1>
+        <br />
+        <span>
+          {" "}
+
+
+          <Alert
+      message="Warning"
+      description="No group is assigned to this project yet!"
+      type="info"
+      showIcon
       
-          <div>
-            <h1>{Projects.Title}</h1>
-            <br />
-            <span>
-              {" "}
-              <Collapse defaultActiveKey={["1"]}>
-                <Panel header="Group 1" key="1">
-                  <p>
-                    {" "}
-                    <List
-                      itemLayout="horizontal"
-                      dataSource={data}
-                      renderItem={(item) => (
-                        <List.Item>
-                          <List.Item.Meta
-                            avatar={<TeamOutlined />}
-                            title={
-                              <a href="https://ant.design">{item.title}</a>
-                            }
-                            description={item.id}
-                          />
-                        </List.Item>
-                      )}
-                    />
-                  </p>
-                </Panel>
-              </Collapse>
-              <Button
-                type="primary"
-                onClick={showGroupDrawer}
-                style={{
-                  backgroundColor: "0092ff",
-                  borderColor: "#0092ff",
-                  borderRadius: "500",
-                  marginTop: "20px",
-                }}
-                icon={
-                  <AppstoreAddOutlined
-                    style={{ fontSize: 20, fontWeight: "bold", color: "white" }}
-                  />
-                }
-              >
-                Assign Project To Group
+    />
+
+          <Collapse defaultActiveKey={["1"]}>
+            <Panel header="Group 1" key="1">
+              <p>
+                {" "}
+                <List
+                  itemLayout="horizontal"
+                  dataSource={data}
+                  renderItem={(item) => (
+                    <List.Item>
+                      <List.Item.Meta
+                        avatar={<TeamOutlined />}
+                        title={<a href="https://ant.design">{item.title}</a>}
+                        description={item.id}
+                      />
+                    </List.Item>
+                  )}
+                />
+              </p>
+            </Panel>
+          </Collapse>
+          <Button
+            type="primary"
+            onClick={showGroupDrawer}
+            style={{
+              backgroundColor: "0092ff",
+              borderColor: "#0092ff",
+              borderRadius: "500",
+              marginTop: "20px",
+            }}
+            icon={
+              <AppstoreAddOutlined
+                style={{ fontSize: 20, fontWeight: "bold", color: "white" }}
+              />
+            }
+          >
+            Assign Project To Group
+          </Button>
+        </span>
+
+        <Drawer
+          title="Assign Project To Group "
+          width={720}
+          onClose={GrouponClose}
+          visible={Groupvisible}
+          bodyStyle={{ paddingBottom: 80 }}
+          extra={
+            <Space>
+              <Button onClick={GrouponClose}>Cancel</Button>
+              <Button onClick={GrouponClose} type="primary">
+                Assign
               </Button>
-            </span>
-
-            <Drawer
-              title="Assign Project To Group "
-              width={720}
-              onClose={GrouponClose}
-              visible={Groupvisible}
-              bodyStyle={{ paddingBottom: 80 }}
-              extra={
-                <Space>
-                  <Button onClick={GrouponClose}>Cancel</Button>
-                  <Button onClick={GrouponClose} type="primary">
-                    Assign
-                  </Button>
-                </Space>
-              }
-            >
-              <Form
-                layout="vertical"
-                hideRequiredMark
-                style={{ paddingLeft: 70 }}
-              >
-                <div className="GroupLabel">
-                  Which Group do you want to assign to this project?:
-                </div>
-                <div className="RadioGroup">
-                  {" "}
-                  <Row gutter={12}>
-                    <Col span={18}>
-                      <Form.Item
-                        name="radio-button"
-                        label=" "
-                        rules={[
-                          { required: true, message: "Please pick a Group!" },
-                        ]}
-                      >
-                        <Radio.Group style={{  marginBlock:30,}}>
-                          <Radio.Button value="Group 1">Group 1</Radio.Button>
-                          <Radio.Button value="Group 2">Group 2</Radio.Button>
-                          <Radio.Button value="Group 3">Group 3</Radio.Button>
-                        </Radio.Group>
-                      </Form.Item>
-                    </Col>
-                  </Row>
-                </div>
-                <Collapse defaultActiveKey={["1"]} style={{width:500}}>
-                <Panel header="Group 1" key="1">
-                  <p>
-                    {" "}
-                    <List
-                      itemLayout="horizontal"
-                      dataSource={data}
-                      renderItem={(item) => (
-                        <List.Item>
-                          <List.Item.Meta
-                            avatar={<TeamOutlined />}
-                            title={
-                              <a href="https://ant.design">{item.title}</a>
-                            }
-                            description={item.id}
-                          />
-                        </List.Item>
-                      )}
-                    />
-                  </p>
-                </Panel>
-              </Collapse>
-              </Form>
-            </Drawer>
-
-            <h3>Project ID: {Projects.id}</h3>
-            <p>
+            </Space>
+          }
+        >
+          <Form layout="vertical" hideRequiredMark style={{ paddingLeft: 70 }}>
+            <div className="GroupLabel">
+              Which Group do you want to assign to this project?:
+            </div>
+            <div className="RadioGroup">
               {" "}
-              {Projects.description}...Lorem ipsum dolor sit amet, consectetur
-              adipiscing elit. Curabitur tristique quam sed tristique molestie.
-              Ut maximus dui et felis egestas rutrum. Praesent nec erat aliquet,
-              congue eros non, imperdiet lorem. Quisque libero nisi, faucibus a
-              felis eu, fermentum posuere ex. Quisque nec ex leo.
-            </p>
-            <br />
-            <br />
-
-            <div className="site-card-wrapper">
-              <Row gutter={16}>
-                <Col span={8}>
-                  <Card.Grid
-                    title="Work Summary"
-                    bordered={true}
-                    style={{
-                      width: 400,
-                      height: 400,
-                      fontWeight: "bold",
-                      fontSize: "25px",
-                      //fontFamily: "cursive",
-                      textAlign: "center",
-                    }}
+              <Row gutter={12}>
+                <Col span={18}>
+                  <Form.Item
+                    name="radio-button"
+                    label=" "
+                    rules={[
+                      { required: true, message: "Please pick a Group!" },
+                    ]}
                   >
-                    <p>Work Summary</p>
-                    <LiquidP />
-                  </Card.Grid>
-                </Col>
-                <Col span={5}></Col>
-                <Col span={8}>
-                  <Card.Grid
-                    title="Project Calendar"
-                    bordered={true}
-                    hoverable={true}
-                    style={{
-                      width: 400,
-                      height: 400,
-                      fontWeight: "bold",
-                      fontSize: "25px",
-                      //fontFamily: "cursive",
-                      textAlign: "center",
-                    }}
-                  >
-                    <p>Project Calendar</p>
-                    <Calendar />
-                  </Card.Grid>
+                    <Radio.Group style={{ marginBlock: 30 }}>
+                      <Radio.Button value="Group 1">Group 1</Radio.Button>
+                      <Radio.Button value="Group 2">Group 2</Radio.Button>
+                      <Radio.Button value="Group 3">Group 3</Radio.Button>
+                    </Radio.Group>
+                  </Form.Item>
                 </Col>
               </Row>
             </div>
-          </div>
+            <Collapse defaultActiveKey={["1"]} style={{ width: 500 }}>
+              <Panel header="Group 1" key="1">
+                <p>
+                  {" "}
+                  <List
+                    itemLayout="horizontal"
+                    dataSource={data}
+                    renderItem={(item) => (
+                      <List.Item>
+                        <List.Item.Meta
+                          avatar={<TeamOutlined />}
+                          title={<a href="https://ant.design">{item.title}</a>}
+                          description={item.id}
+                        />
+                      </List.Item>
+                    )}
+                  />
+                </p>
+              </Panel>
+            </Collapse>
+          </Form>
+        </Drawer>
 
-          /************************************************************/
-        
-      
+        <h3>Project ID: {Projects.id}</h3>
+        <p>
+          {" "}
+          {Projects.description}...Lorem ipsum dolor sit amet, consectetur
+          adipiscing elit. Curabitur tristique quam sed tristique molestie. Ut
+          maximus dui et felis egestas rutrum. Praesent nec erat aliquet, congue
+          eros non, imperdiet lorem. Quisque libero nisi, faucibus a felis eu,
+          fermentum posuere ex. Quisque nec ex leo.
+        </p>
+        <br />
+        <br />
 
+        <div className="site-card-wrapper">
+          <Row gutter={16}>
+            <Col span={8}>
+              <Card.Grid
+                title="Work Summary"
+                bordered={true}
+                style={{
+                  width: 400,
+                  height: 400,
+                  fontWeight: "bold",
+                  fontSize: "25px",
+                  //fontFamily: "cursive",
+                  textAlign: "center",
+                }}
+              >
+                <p>Work Summary</p>
+                <LiquidP />
+              </Card.Grid>
+            </Col>
+            <Col span={5}></Col>
+            <Col span={8}>
+              <Card.Grid
+                title="Project Calendar"
+                bordered={true}
+                hoverable={true}
+                style={{
+                  width: 400,
+                  height: 400,
+                  fontWeight: "bold",
+                  fontSize: "25px",
+                  //fontFamily: "cursive",
+                  textAlign: "center",
+                }}
+              >
+                <p>Project Calendar</p>
+                <Calendar />
+              </Card.Grid>
+            </Col>
+          </Row>
+        </div>
+      </div>
+      /************************************************************/
       <TasksLists />
       <Row justify="end">
         <Col span={4}>
