91739589acb45172ffc4f4d0398d37be85a3d6c7
ShefaaAbed <101830221+ShefaaAbed@users.noreply.github.com>
2022-05-25 11:37:37 +0300

    Add functionality to Donut chart.

--- src/components/Dashboard/TaskDonut.jsx
+++ src/components/Dashboard/TaskDonut.jsx
@@ -8,63 +8,63 @@ import { Typography } from 'antd';
 const { Title } = Typography;
 
 export default function TaskDonut() {
-    const data = [
-        {
-            type: 'Assigned',
-            value: 42,
-        },
-        {
-            type: 'Working on',
-            value: 30,
-        },
+	const data = [
+		{
+			type: 'Assigned',
+			value: 42,
+		},
+		{
+			type: 'Working on',
+			value: 30,
+		},
 
-        {
-            type: 'Finished',
-            value: 18,
-        },
-    ];
-    const config = {
-        appendPadding: 30,
-        data,
-        angleField: 'value',
-        colorField: 'type',
-        radius: 1,
-        innerRadius: 0.6,
-        label: {
-            type: 'inner',
-            offset: '-50%',
-            content: '{value}',
-            style: {
-                textAlign: 'center',
-                fontSize: 14,
-            },
-        },
-        interactions: [
-            {
-                type: 'element-selected',
-            },
-            {
-                type: 'element-active',
-            },
-        ],
-        statistic: {
-            title: false,
-            content: {
-                style: {
-                    whiteSpace: 'pre-wrap',
-                    overflow: 'hidden',
-                    textOverflow: 'ellipsis',
-                },
-                content: '',
-            },
-        },
-    };
+		{
+			type: 'Finished',
+			value: 18,
+		},
+	];
+	const config = {
+		appendPadding: 30,
+		data,
+		angleField: 'value',
+		colorField: 'type',
+		radius: 1,
+		innerRadius: 0.6,
+		label: {
+			type: 'inner',
+			offset: '-50%',
+			content: '{value}',
+			style: {
+				textAlign: 'center',
+				fontSize: 14,
+			},
+		},
+		interactions: [
+			{
+				type: 'element-selected',
+			},
+			{
+				type: 'element-active',
+			},
+		],
+		statistic: {
+			title: false,
+			content: {
+				style: {
+					whiteSpace: 'pre-wrap',
+					overflow: 'hidden',
+					textOverflow: 'ellipsis',
+				},
+				content: '',
+			},
+		},
+	};
 
-    return (
-        <Card style={{ backgroundColor: '#F7F7F7', height: '100%' }}>
-            <Title level={2}>Tasks summary</Title>
-            <Divider />
-            <Pie {...config} />
-        </Card>
-    );
+	return (
+		<Card style={{ backgroundColor: '#F7F7F7', height: '100%' }}>
+			<Title level={2}>Tasks summary</Title>
+			<Divider />
+			<Pie {...config} />
+		</Card>
+	);
 }
