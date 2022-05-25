52afd9e4a1e3da51b1f77a956f4defa205d98bdf
Jafar <aljuneidi.jafar@gmail.com>
2022-05-25 15:13:03 +0300

    created individual task component

---
+++ src/components/Project/Task.jsx
@@ -0,0 +1,104 @@
+import React, { useState } from 'react';
+import { Form, Modal, Button, DatePicker, Input } from 'antd';
+
+const TaskModal = ({ visible, onCreate, onCancel, confirmLoading }) => {
+  const [form] = Form.useForm();
+  return (
+    <Modal
+      width={700}
+      visible={visible}
+      title='Create a new Project'
+      okText='Create'
+      cancelText='Cancel'
+      confirmLoading={confirmLoading}
+      onCancel={() => {
+        form.resetFields();
+        onCancel();
+      }}
+      onOk={() => {
+        form
+          .validateFields()
+          .then((values) => {
+            onCreate(values);
+            form.resetFields();
+          })
+          .catch((info) => {
+            console.log('Validate Failed:', info);
+          });
+      }}
+    >
+      <Form
+        form={form}
+        labelCol={{
+          span: 5,
+        }}
+        wrapperCol={{
+          span: 19,
+        }}
+        layout='horizontal'
+      >
+        <Form.Item name='title' label='Title'>
+          <Input />
+        </Form.Item>
+
+        <Form.Item
+          name='description'
+          label='Description'
+          rules={[
+            { required: true, message: 'Please input project description' },
+          ]}
+        >
+          <Input.TextArea showCount maxLength={200} />
+        </Form.Item>
+
+        <Form.Item name='dueDate' label='Due Date'>
+          <DatePicker />
+        </Form.Item>
+      </Form>
+    </Modal>
+  );
+};
+
+export default function Task({ provided, snapshot, task }) {
+  const [visible, setVisible] = useState(false);
+  const [confirmLoading, setConfirmLoading] = useState(false);
+
+  const onCreate = () => {
+    console.log('create');
+  };
+
+  return (
+    <>
+      <div
+        onClick={() => setVisible(true)}
+        ref={provided.innerRef}
+        {...provided.draggableProps}
+        {...provided.dragHandleProps}
+        style={{
+          userSelect: 'none',
+          padding: '0.5rem',
+          margin: '0 0 0.5rem 0',
+          background: snapshot.isDragging ? '#4a9cc2' : '#5cc2f2',
+          color: 'white',
+          //   display: 'inline-block',
+          //   width: '120px',
+          textAlign: 'center',
+          borderRadius: '5px',
+          minHeight: '50px',
+          whiteSpace: 'nowrap',
+          ...provided.draggableProps.style,
+        }}
+      >
+        {task.title}
+      </div>
+      <TaskModal
+        visible={visible}
+        onCreate={onCreate}
+        confirmLoading={confirmLoading}
+        onCancel={() => {
+          setVisible(false);
+        }}
+      />
+    </>
+  );
+}
--- src/components/Project/TaskList.jsx
+++ src/components/Project/TaskList.jsx
@@ -1,9 +1,11 @@
 import React, { useEffect, useState } from 'react';
-import { Space, Typography, Row, Col } from 'antd';
+import { Space, Typography } from 'antd';
 import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
 import Loader from '../Loader';
 import AddTask from './AddTask';
+import Task from './Task';
 import { v4 as uuidv4 } from 'uuid';
+import { updateProject } from '../../firestore/projects';
 
 const { Title } = Typography;
 export default function TaskList({ tasks, projectId, getNewData }) {
@@ -32,24 +34,83 @@ export default function TaskList({ tasks, projectId, getNewData }) {
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    setColumns([
-      ...columns.map((column) => {
-        tasks.forEach((task) => {
-          if (task.status === column.name) column.tasks.push(task);
-        });
-        return column;
-      }),
-    ]);
-    console.log(columns);
+    const getData = () => {
+      setColumns((columns) => [
+        ...columns.map((column) => {
+          tasks.forEach((task) => {
+            if (task.status === column.name) column.tasks.push(task);
+          });
+          return column;
+        }),
+      ]);
+    };
+
+    getData();
     setLoading(false);
-  }, []);
+  }, [tasks]);
+
+  const onDragEnd = (result) => {
+    console.log(result);
+    if (!result.destination) return;
+    const { source, destination } = result;
+
+    if (source.droppableId === destination.droppableId) {
+      const column = columns.find(
+        (c) => c.name === destination.droppableId
+      ).tasks;
+
+      const copiedItems = [...column];
+      const [removed] = copiedItems.splice(source.index, 1);
+      copiedItems.splice(destination.index, 0, removed);
+      setColumns(
+        columns.map((column) => {
+          if (column.name === source.droppableId) {
+            return { ...column, tasks: copiedItems };
+          }
+          return column;
+        })
+      );
+    } else {
+      const sourceColumn = columns.find(
+        (c) => c.name === source.droppableId
+      ).tasks;
+
+      const destinationColumn = columns.find(
+        (c) => c.name === destination.droppableId
+      ).tasks;
+
+      const sourceItems = [...sourceColumn];
+      const destinationItems = [...destinationColumn];
+      const [removed] = sourceItems.splice(source.index, 1);
+      destinationItems.splice(destination.index, 0, removed);
+
+      tasks.forEach((task) => {
+        if (task.id === removed.id) {
+          task.status = destination.droppableId;
+        }
+      });
+      updateProject(projectId, { tasks });
+
+      setColumns(
+        columns.map((column) => {
+          if (column.name === source.droppableId) {
+            return { ...column, tasks: sourceItems };
+          } else if (column.name === destination.droppableId) {
+            return { ...column, tasks: destinationItems };
+          }
+          return column;
+        })
+      );
+    }
+  };
+
   return (
     <>
       <Space
         align='baseline'
         style={{
           display: 'flex',
-          justifyContent: 'space-between',
+          justifyContent: 'space-around',
           alignItems: 'center',
           width: '100%',
         }}
@@ -71,58 +132,51 @@ export default function TaskList({ tasks, projectId, getNewData }) {
           minHeight: 500,
         }}
       >
-        <DragDropContext onDragEnd={(result) => console.log(result)}>
+        <DragDropContext onDragEnd={(result) => onDragEnd(result)}>
           {loading ? (
             <Loader />
           ) : (
             columns.map((column) => (
-              <Droppable key={column.id} droppableId={column.id}>
-                {(provided, snapshot) => (
-                  <div
-                    {...provided.droppableProps}
-                    ref={provided.innerRef}
-                    style={{
-                      background: snapshot.isDraggingOver
-                        ? 'lightblue'
-                        : 'lightgrey',
-                      padding: 4,
-                      margin: 4,
-                      width: 250,
-                      minHeight: 500,
-                    }}
-                  >
-                    {column.tasks.map((task, index) => (
-                      <Draggable
-                        key={task.id}
-                        draggableId={task.id}
-                        index={index}
-                      >
-                        {(provided, snapshot) => (
-                          <div
-                            ref={provided.innerRef}
-                            {...provided.draggableProps}
-                            {...provided.dragHandleProps}
-                            style={{
-                              userSelect: 'none',
-                              padding: 8,
-                              margin: '0 0 8px 0',
-                              minHeight: '50px',
-                              background: snapshot.isDragging
-                                ? '#263b4a'
-                                : '#456c86',
-                              color: 'white',
-                              ...provided.draggableProps.style,
-                            }}
-                          >
-                            {task.title}
-                          </div>
-                        )}
-                      </Draggable>
-                    ))}
-                    {provided.placeholder}
-                  </div>
-                )}
-              </Droppable>
+              <div key={column.name}>
+                <Title level={5} style={{ textAlign: 'center' }}>
+                  {column.name}
+                </Title>
+
+                <Droppable key={column.name} droppableId={column.name}>
+                  {(provided, snapshot) => (
+                    <div
+                      {...provided.droppableProps}
+                      ref={provided.innerRef}
+                      style={{
+                        background: snapshot.isDraggingOver
+                          ? 'lightblue'
+                          : 'lightgrey',
+                        padding: 4,
+                        margin: 4,
+                        minWidth: 200,
+                        minHeight: 500,
+                      }}
+                    >
+                      {column.tasks.map((task, index) => (
+                        <Draggable
+                          key={task.id}
+                          draggableId={task.id}
+                          index={index}
+                        >
+                          {(provided, snapshot) => (
+                            <Task
+                              provided={provided}
+                              snapshot={snapshot}
+                              task={task}
+                            />
+                          )}
+                        </Draggable>
+                      ))}
+                      {provided.placeholder}
+                    </div>
+                  )}
+                </Droppable>
+              </div>
             ))
           )}
         </DragDropContext>
