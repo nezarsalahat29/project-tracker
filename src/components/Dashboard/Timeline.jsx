035f131dff861f81211386f61c14d56315b4aaa8
Suhaib Atef <suhaibatef20@gmail.com>
2022-05-26 08:59:11 +0300

    Timeline Firebase Integeration

--- src/components/Dashboard/Timeline.jsx
+++ src/components/Dashboard/Timeline.jsx
@@ -1,29 +1,92 @@
-import React from 'react';
-import { Card, Timeline } from 'antd';
-import { Divider } from 'antd';
-import { Typography } from 'antd';
-
+import React, { useEffect, useState } from "react";
+import { Card, Timeline } from "antd";
+import { Divider } from "antd";
+import { Typography } from "antd";
+import { getProject, getAllProjects } from "../../firestore/projects";
+import { useAuth } from "../../contexts/AuthContext";
+import { getGroupFromDb } from "../../firestore/groups";
 const { Title } = Typography;
+let a = [{ day: "numeric" }, { month: "short" }, { year: "numeric" }];
+function join(t, a, s) {
+  function format(m) {
+    let f = new Intl.DateTimeFormat("en", m);
+    return f.format(t);
+  }
+  return a.map(format).join(s);
+}
+function getData(projects) {
+  var data = {};
+  projects.forEach((element) => {
+    element.tasks.forEach((task) => {
+      data[Math.floor(task.dueDate.seconds / (3600 * 24))] = data[
+        Math.floor(task.dueDate.seconds / (3600 * 24))
+      ]
+        ? [
+            ...data[Math.floor(task.dueDate.seconds / (3600 * 24))],
+            "Task: " + task.title,
+          ]
+        : ["Task: " + task.title];
+    });
+    element.deliverables.forEach((deliverable) => {
+      data[Math.floor(deliverable.dueDate.seconds / (3600 * 24))] = data[
+        Math.floor(deliverable.dueDate.seconds / (3600 * 24))
+      ]
+        ? [
+            ...data[Math.floor(deliverable.dueDate.seconds / (3600 * 24))],
+            "Deliverable: " + deliverable.title,
+          ]
+        : ["Deliverable: " + deliverable.title];
+    });
+  });
+  return data;
+}
 
 function TimelineComponent() {
-    return (
-        <Card style={{ backgroundColor: '#F7F7F7', height: '100%' }}>
-            <Title level={2}>TimeLine</Title>
-            <Divider />
-            <Timeline>
-                <Timeline.Item>
-                    Create a Requirement File 2022-01-15
-                </Timeline.Item>
-                <Timeline.Item>Create a Design File 2022-02-07</Timeline.Item>
-                <Timeline.Item>
-                    <p>Design the database</p>
-                    <p>Design the user interface 2022-05-15</p>
-                </Timeline.Item>
-                <Timeline.Item>
-                    Deliver the full web application 2022-06-30
-                </Timeline.Item>
-            </Timeline>
-        </Card>
-    );
+  const { currentUser } = useAuth();
+  const [data, setData] = useState([]);
+
+  const getProjectData = async () => {
+    var dataFetched = [];
+    const groups = await getGroupFromDb(currentUser.groupId);
+    const projectData = currentUser.instructor
+      ? await getAllProjects()
+      : [await getProject(groups.projectID)];
+    const d = getData(projectData);
+    for (const property in d) {
+      dataFetched.push([
+        join(new Date(property * (3600 * 24) * 1000), a, "-"),
+        [...d[property]],
+      ]);
+    }
+    setData(dataFetched);
+  };
+
+  useEffect(() => {
+    const getData = async () => {
+      await Promise.all([getProjectData()]);
+    };
+
+    getData();
+  }, []);
+
+  return (
+    <Card style={{ backgroundColor: "#F7F7F7", height: "100%" }}>
+      <Title level={2}>TimeLine</Title>
+      <Divider />
+      <Timeline>
+        {data &&
+          data.map((item) => {
+            return (
+              <Timeline.Item>
+                <p>{item[0]}</p>
+                {item[1].map((item2) => (
+                  <p>{item2}</p>
+                ))}
+              </Timeline.Item>
+            );
+          })}
+      </Timeline>
+    </Card>
+  );
 }
 export default TimelineComponent;
